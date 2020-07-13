e6e0cbf3d40417150df1361913f1fe4907b04691
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 14:59:33 +0200

    FEAT: add some additional seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Emptying DB..'
+Nerd.destroy_all
+Link.destroy_all
+LinkLike.destroy_all
 
-nerd = Nerd.create!(email: 'test@mail.com', password: 'password')
+puts 'Creating nerds'
+nerd1 = Nerd.create!(email: 'test@mail.com', password: 'password')
+nerd2 = Nerd.create!(email: 'test1@mail.com', password: 'password')
 
-link1 = Link.create!(url: 'https://www.imdb.com/title/tt0371724/', title: '42')
-link2 = Link.create!(url: 'https://www.imdb.com/title/tt0487831/', title: 'Have you tried turning it off and on again?')
+puts 'Creating links'
+link1 = Link.create!(url: 'https://www.imdb.com/title/tt0371724', title: '42')
+link2 = Link.create!(url: 'https://www.imdb.com/title/tt0487831', title: 'Have you tried turning it off and on again?')
 link3 = Link.create!(url: 'https://chucknorrisfacts.net/random-fact', title: 'chucknorrisfacts')
+link4 = Link.create!(url: 'https://nl.ign.com/', title: 'game stuff')
 
-LinkLike.create!(nerd: nerd, link: link1)
+puts 'Creating likes'
+LinkLike.create!(nerd: nerd1, link: link1)
+LinkLike.create!(nerd: nerd1, link: link2)
+LinkLike.create!(nerd: nerd2, link: link1)
+LinkLike.create!(nerd: nerd2, link: link3)
+LinkLike.create!(nerd: nerd2, link: link4)
+
 
