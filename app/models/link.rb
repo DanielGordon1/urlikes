0881d29802eaa6abb1bd24f95a074d1c182d854c
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 15:00:33 +0200

    FEAT: add dep destroy and sorted_by_votes scope

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,9 +2,15 @@
 
 # Link model to be created and liked by Nerds.
 class Link < ApplicationRecord
-  has_many :link_likes
+  has_many :link_likes, dependent: :destroy
   has_many :liking_nerds, through: :link_likes, source: :nerd
 
   validates :title, presence: true, uniqueness: true
   validates :url, presence: true, uniqueness: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
+
+  scope :sorted_by_votes, -> { joins(:link_likes).order('COUNT(link_likes.id)').group('links.id') }
+
+  def like_count
+    link_likes.count
+  end
 end
