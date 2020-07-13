690fedfb9dcda2a7435327163b4d429277fb4196
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 20:22:04 +0200

    FEAT: add some basic req specs for links

--- spec/requests/links_request_spec.rb
+++ spec/requests/links_request_spec.rb
@@ -1,5 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Links", type: :request do
+RSpec.describe 'Links', type: :request do
+  let(:nerd) { create(:nerd) }
+  let(:link_params) do
+    { link: { title: 'some random post', url: 'https://www.check-out-this.com' } }
+  end
+  describe '#index' do
+    it 'returns a valid status code' do
+      login_as(nerd)
+      get root_path
+      expect(response.status).to eq(200)
+    end
+  end
 
+  describe '#create' do
+    it 'returns a valid status code and creates a link' do
+      expect(Link.count).to eq(0)
+      login_as(nerd)
+
+      post('/links', params: link_params)
+      expect(response.status).to eq(302)
+      expect(Link.count).to eq(1)
+    end
+  end
 end
