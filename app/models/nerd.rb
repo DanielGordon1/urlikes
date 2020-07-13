cb28952d2bf57f3a4a66721db360922960f15762
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 15:41:39 +0200

    FEAT: add class method to Nerd to create object from google data.

--- app/models/nerd.rb
+++ app/models/nerd.rb
@@ -11,4 +11,8 @@ class Nerd < ApplicationRecord
   has_many :liked_links, through: :link_likes, source: :link
 
   validates :email, presence: true, uniqueness: true
+
+  def self.from_google(email:, full_name:, uid:, avatar_url:)
+    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
+  end
 end
