0906c2237e6b428c06f98074a8921f577ba37d00
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 12:14:20 +0200

    WIP: start with config for oauth - add model devise options + pass google auth data in devise initializer

--- app/models/nerd.rb
+++ app/models/nerd.rb
@@ -5,7 +5,8 @@ class Nerd < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :link_likes
   has_many :liked_links, through: :link_likes, source: :link
 
