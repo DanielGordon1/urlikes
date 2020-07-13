c7a0f79fdabba83a605b283dbfeaaf05d01ed33b
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 13:37:55 +0200

    FEAT: add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+nerd = Nerd.create!(email: 'test@mail.com', password: 'password')
+
+link1 = Link.create!(url: 'https://www.imdb.com/title/tt0371724/', title: '42')
+link2 = Link.create!(url: 'https://www.imdb.com/title/tt0487831/', title: 'Have you tried turning it off and on again?')
+link3 = Link.create!(url: 'https://chucknorrisfacts.net/random-fact', title: 'chucknorrisfacts')
+
+LinkLike.create!(nerd: nerd, link: link1)
+
