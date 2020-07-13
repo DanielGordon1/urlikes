ee2f7db7e063bfba0b323d678296a1c7805ffe1b
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 11:20:34 +0200

    FEAT: tweak nerd factory and add first spec.

--- spec/factories/nerds.rb
+++ spec/factories/nerds.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :nerd do
-    sequence(:email) { |n| "test-#{n.to_s.rjust(3, '0')}@sample.com" }
-    password { "123456" }
+    email { Faker::Internet.email }
+    password { "password" }
   end
 end
--- spec/models/nerd_spec.rb
+++ spec/models/nerd_spec.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Nerd, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'valid records' do
+    it 'has a valid factory' do
+      expect(build(:nerd)).to be_valid
+    end
+  end
 end
