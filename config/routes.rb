bddfd9da120c7bc8d526b32e4ac69b7b71ac6063
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 16:48:21 +0200

    FEAT: create entrypoint for patch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: "links#index"
   devise_for :nerds, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  resources :links, only: [] do
+    resources :link_likes, only: :create
+  end
+  resources :link_likes, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ puts 'Creating links'
 link1 = Link.create!(url: 'https://www.imdb.com/title/tt0371724', title: '42')
 link2 = Link.create!(url: 'https://www.imdb.com/title/tt0487831', title: 'Have you tried turning it off and on again?')
 link3 = Link.create!(url: 'https://chucknorrisfacts.net/random-fact', title: 'chucknorrisfacts')
-link4 = Link.create!(url: 'https://nl.ign.com/', title: 'game stuff')
+link4 = Link.create!(url: 'https://nl.ign.com/', title: 'IGN Benelux - game tings', created_at: Date.today - 1)
 
 puts 'Creating likes'
 LinkLike.create!(nerd: nerd1, link: link1)
