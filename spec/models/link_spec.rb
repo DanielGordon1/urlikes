be4a1740b7785c752d16c6f766bc8bae2e0c1449
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 11:38:49 +0200

    FEAT: add specs for links

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'valid records' do
+    it 'has a valid factory' do
+      expect(build(:link)).to be_valid
+    end
+
+    it { should validate_presence_of(:title) }
+
+    it { should have_many(:link_likes) }
+    it { should have_many(:liking_nerds) }
+  end
 end
