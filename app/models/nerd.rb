ee23f2380b6111118e26db3b406cde582eec1290
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 12:11:15 +0200

    CLEAN: switch associations & validations order to be consisten

--- app/models/nerd.rb
+++ app/models/nerd.rb
@@ -6,7 +6,8 @@ class Nerd < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, presence: true, uniqueness: true
   has_many :link_likes
   has_many :liked_links, through: :link_likes, source: :link
+
+  validates :email, presence: true, uniqueness: true
 end
