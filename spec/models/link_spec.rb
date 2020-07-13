7613aa8563e7a90827e8bd5b70bdaa18f2a27068
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 13:42:54 +0200

    FEAT: add spec for like_count

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
+  let(:nerd) { build(:nerd) }
+  let(:link) { build(:link) }
+  let(:like) { build(:link_like, link: link, nerd: nerd) }
+
   describe 'valid records' do
     it 'has a valid factory' do
       expect(build(:link)).to be_valid
@@ -17,5 +21,15 @@ RSpec.describe Link, type: :model do
 
     it { should have_many(:link_likes) }
     it { should have_many(:liking_nerds) }
+  end
+
+  describe '#like_count' do
+    it 'should return the amount of likes this link has received' do
+      expect(link.like_count).to eq(0)
+
+      like.save
+
+      expect(link.like_count).to eq(1)
+    end
   end
 end
