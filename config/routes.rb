6dcd6e53729bcfb0e113df95c50f1d45dca8bdae
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 15:40:38 +0200

    FEAT: replace nested structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "links#index"
-  devise_for :nerds, controllers: { omniauth_callbacks: 'nerds/omniauth_callbacks' }
+  devise_for :nerds, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 end
