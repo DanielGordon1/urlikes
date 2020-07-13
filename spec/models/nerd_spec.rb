9f37948303dd94c7a9fb574f481682d0f3dfb421
Daniel Gordon <d_gordon1@icloud.com>
2020-07-13 11:30:45 +0200

    FEAT: add shoulda matchers

--- spec/models/nerd_spec.rb
+++ spec/models/nerd_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Nerd, type: :model do
     it 'has a valid factory' do
       expect(build(:nerd)).to be_valid
     end
+
+    it { should validate_presence_of(:email) }
+
+    it { should have_many(:link_likes) }
+    it { should have_many(:liked_links) }
   end
 end
