88d9ec522e52ef0578aee46d395d2a8f2766dd6b
Daniel Gordon <d_gordon1@icloud.com>
2020-07-14 09:03:35 +0200

    FEAT: add spec for from google method

--- spec/models/nerd_spec.rb
+++ spec/models/nerd_spec.rb
@@ -3,6 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Nerd, type: :model do
+  let(:google_data) do
+    {
+      email: Faker::Internet.email,
+      full_name: Faker::TvShows::RickAndMorty.character,
+      uid: Faker::Number.number(digits: 10),
+      avatar_url: Faker::Avatar.image
+    }
+  end
   describe 'valid records' do
     it 'has a valid factory' do
       expect(build(:nerd)).to be_valid
@@ -13,5 +21,20 @@ RSpec.describe Nerd, type: :model do
     it { should have_many(:link_likes) }
     it { should have_many(:liked_links) }
     it { should have_many(:links) }
+  end
+
+  describe '#from_google' do
+    it 'should create a user when email is not in use yet' do
+      expect(Nerd.count).to eq(0)
+
+      nerd = Nerd.from_google(**google_data)
+
+      expect(Nerd.count).to eq(1)
+      expect(nerd.persisted?).to eq(true)
+      expect(Nerd.first).to eq(nerd)
+    end
+
+    it 'should enrich a user with google data when an email is already in use' do
+    end
   end
 end
